876a4f135758d92cb9c149587dd63ebefc3bcf21
CVandiedonck <claire.vandiedonck@u-paris.fr>
2020-06-10 18:58:32 +0200

    update sscript with correct paths from TP_GWAS and add also generated outputs

--- TP_GWAS/tp_gwas_commands.sh
+++ TP_GWAS/tp_gwas_commands.sh
@@ -1,15 +1,15 @@
 # Merge cases and controls files
-plink -bfile input/ic_cases --make-bed --out data/ic_cases
-plink -bfile input/ic_controls --make-bed --out data/ic_controls
-plink --bfile input/ic_cases --exclude input/toexclude.txt --make-bed --out data/ic_cases_autosomes
-plink --bfile input/ic_controls --bmerge data/ic_cases_autosomes.bed data/ic_cases_autosomes.bim data/ic_cases_autosomes.fam --make-bed --out data/t1dcc
-plink --bfile input//ic_controls --exclude input/rs3754055.txt --make-bed --out input/ic_controls2
-plink --bfile input/ic_cases_autosomes --exclude input/rs3754055.txt --make-bed --out input/ic_cases_autosomes2
-plink --bfile input/ic_controls --flip data/t1dcc-merge.missnp --make-bed --out data/ic_controls_flipped
-plink --bfile input/ic_controls_flipped --bmerge input/ic_cases_autosomes2.bed input/ic_cases_autosomes2.bim input/ic_cases_autosomes2.fam --make-bed --out data/t1dcc
+plink -bfile input/ic_cases --make-bed --out output/ic_cases
+plink -bfile input/ic_controls --make-bed --out output/ic_controls
+plink --bfile output/ic_cases --exclude input/toexclude.txt --make-bed --out output/ic_cases_autosomes
+plink --bfile output/ic_controls --bmerge output/ic_cases_autosomes.bed output/ic_cases_autosomes.bim output/ic_cases_autosomes.fam --make-bed --out output/t1dcc
+plink --bfile output/ic_controls --exclude input/rs3754055.txt --make-bed --out input/ic_controls2
+plink --bfile output/ic_cases_autosomes --exclude input/rs3754055.txt --make-bed --out input/ic_cases_autosomes2
+plink --bfile output/ic_controls --flip output/t1dcc-merge.missnp --make-bed --out output/ic_controls_flipped
+plink --bfile output/ic_controls_flipped --bmerge input/ic_cases_autosomes2.bed input/ic_cases_autosomes2.bim input/ic_cases_autosomes2.fam --make-bed --out output/t1dcc
 
 # clean SNPs
-plink --bfile input/t1d_ctrl --geno 0.05 --make-bed --out output/filter1
+plink --bfile output/t1dcc --geno 0.05 --make-bed --out output/filter1
 plink --bfile output/filter1 --test-missing --pfilter 0.00001 --out output/missingcc
 plink --bfile output/filter1 --exclude output/missingcc.missing --make-bed --out output/filter2
 plink --bfile output/filter2 --hwe 0.0001 --make-bed --out output/filter3
@@ -22,33 +22,35 @@ king -b output/filter5.bed --kinship --related --prefix output/filter5
 king -b output/filter5.bed --kinship --unrelated --prefix output/filter5
 
 plink --bfile output/filter5 --keep output/filter5unrelated.txt --make-bed --out output/filter6
-# ou NEW : enlevez les individus apparent�s avec:
+# ou NEW : enlevez les individus apparentes avec:
 plink --bfile output/filter5 --remove output/filter5unrelated_toberemoved.txt --make-bed --out output/filter6
 
 # First association test:
 plink --bfile output/filter6 --assoc --adjust --out output/allelictest
 
 # Start R
-install.packages("qqman")
+if (!requireNamespace("qqman", quietly = TRUE)){
+    install.packages("qqman")}
 library(qqman)
+sessionInfo()    
 gwasoutputults <- read.table("output/allelictest.assoc",header=T)
 head(gwasResults)
 png("output/manhattan.png")
 manhattan(gwasResults)
 dev.off()
-png("ouptut/qqplot.png")
+png("output/qqplot.png")
 qq(gwasResults$P, main="Q-Qplot of P-values")
 dev.off()
 quit('no')
 
 # perform MDS analysis
-plink --bfile output/filter6 --exclude data/mhc_range.txt --range --make-bed --out output/withoutmhc
+plink --bfile output/filter6 --exclude input/mhc_range.txt --range --make-bed --out output/withoutmhc
 plink --bfile output/withoutmhc --indep 50 5 2 --out output/indep
 plink --bfile output/withoutmhc --extract output/indep.prune.in --make-bed --out output/withoutld
 king -b output/withoutld.bed --mds --ibs --prefix output/stratifwithoutld
 
 # Start R
-mds <- read.table("output/stratifwithoutldpc.ped", header=F)
+mds <- read.table("output/stratifwithoutldpc.txt", header=F)
 png("output/mds.png")
 par(mfrow=c(2,2))
 plot(mds$V7, mds$V8, type="n", main="MDS vectors 1 and 2 in Controls")
@@ -60,7 +62,7 @@ points(mds$V8[1:357], mds$V9[1:357], col="black")
 plot(mds$V8, mds$V9, type="n", main="MDS vectors 2 and 3 in Cases")
 points(mds$V8[358:577], mds$V9[358:577], col="red")
 dev.off()
-quit(�no�)
+quit("no")
 
 # 2nd association test, after MDS:
 plink --bfile output/filter6 --logistic hide-covar --adjust --covar output/stratifwithoutldpc.txt --covar-number 7-11 --ci 0.95 --out output/logistic_mds_PC5
@@ -78,7 +80,7 @@ dev.off()
 quit('no')
 
 # trios analysis
-plink --bfile data/ic_trio --tdt --adjust --out output/tdttrio
+plink --bfile input/ic_trio --tdt --adjust --out output/tdttrio
 
 # Start R
 library(qqman)
